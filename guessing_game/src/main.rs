ba70081f834b3ae2db07ff730b9b8bcf3742bc35
Roman Meredith <romanmeredith@gmail.com>
2024-03-21 23:26:39 -0600

    first commit

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,36 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("Guess the number!");
+    println!("Please input your guess.");
+
+
+
+    println!("Guess the number!");
+
+    println!("secret number: {secret_number}");
+    loop {
+        let mut guess = String::new();
+        println!("Please Guess the number!");
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("too small!"),
+            Ordering::Greater => println!("too big!"),
+            Ordering::Equal => {
+                println!("you win!");
+                break;
+            },
+        }
+    }
+
+}
